8cd1df8c27a3946cd6f4d9cd292669605089242d
Charles Loomis <loomis@lal.in2p3.fr>
2011-03-27 10:50:14 -0400

    initial skeleton

---
+++ persistent-disks/src/main/java/eu/stratuslab/storage/disk/main/PersistentDiskApplication.java
@@ -0,0 +1,47 @@
+package eu.stratuslab.storage.main;
+
+import java.util.logging.Logger;
+
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.resource.Directory;
+import org.restlet.routing.Router;
+import org.restlet.routing.Template;
+import org.restlet.routing.TemplateRoute;
+
+import eu.stratuslab.storage.disk.resources.DiskResource;
+
+public class PersistentDiskApplication extends Application {
+
+    protected Logger logger = getLogger();
+
+    public PersistentDiskApplication() {
+
+        setName("StratusLab Persistent Disk Server");
+        setDescription("StratusLab server for persistent disk storage.");
+        setOwner("StratusLab");
+        setAuthor("Charles Loomis");
+
+        getTunnelService().setUserAgentTunnel(true);
+
+    }
+
+    @Override
+    public Restlet createInboundRoot() {
+
+        // Create a router Restlet that defines routes.
+        Router router = new Router(getContext());
+
+        Directory indexDir = new Directory(getContext(), "war:///");
+        indexDir.setNegotiatingContent(false);
+        indexDir.setIndexName("index.html");
+
+        // Defines a route for the resource "list of metadata entries"
+        router.attach("/disk", DiskResource.class);
+
+        router.attach("/", indexDir);
+
+        return router;
+    }
+
+}
---
+++ persistent-disks/src/main/java/eu/stratuslab/storage/disk/resources/DiskResource.java
@@ -0,0 +1,47 @@
+/*
+ Created as part of the StratusLab project (http://stratuslab.eu),
+ co-funded by the European Commission under the Grant Agreement
+ INSFO-RI-261552.
+
+ Copyright (c) 2011, Centre National de la Recherche Scientifique (CNRS)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+package eu.stratuslab.storage.disk.resources;
+
+import static org.restlet.data.MediaType.TEXT_PLAIN;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.restlet.Request;
+import org.restlet.data.MediaType;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+import org.w3c.dom.Document;
+
+public class DiskResource extends ServerResource {
+
+    @Get("txt")
+    public Representation toText() {
+        return new StringRepresentation("got to DiskResource toText()", TEXT_PLAIN);
+    }
+
+}
