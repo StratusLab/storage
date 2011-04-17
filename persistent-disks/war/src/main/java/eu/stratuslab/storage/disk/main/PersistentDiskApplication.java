83307f89de09837a4189e0ca467a75623d6f00c1
Charles Loomis <loomis@lal.in2p3.fr>
2011-04-17 19:13:50 +0200

    change default location of diskstore; make it configurable

--- persistent-disks/war/src/main/java/eu/stratuslab/storage/disk/main/PersistentDiskApplication.java
+++ persistent-disks/war/src/main/java/eu/stratuslab/storage/disk/main/PersistentDiskApplication.java
@@ -1,6 +1,9 @@
 package eu.stratuslab.storage.disk.main;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.restlet.Application;
 import org.restlet.Restlet;
@@ -14,9 +17,15 @@ import eu.stratuslab.storage.disk.resources.CreateResource;
 
 public class PersistentDiskApplication extends Application {
 
+    public static final String CFG_FILENAME = "/etc/stratuslab/persistent-disk.cfg";
+
     public static final String DEFAULT_DISK_STORE = "/tmp/diskstore";
 
-    public static final File DISK_STORE = new File(DEFAULT_DISK_STORE);
+    public static final File DISK_STORE;
+
+    static {
+        DISK_STORE = getDiskStoreDirectory();
+    }
 
     public PersistentDiskApplication() {
 
@@ -26,6 +35,34 @@ public class PersistentDiskApplication extends Application {
         setAuthor("Charles Loomis");
 
         getTunnelService().setUserAgentTunnel(true);
+    }
+
+    public static File getDiskStoreDirectory() {
+
+        String diskStoreDir = DEFAULT_DISK_STORE;
+
+        File cfgFile = new File(CFG_FILENAME);
+        if (cfgFile.exists()) {
+            Properties properties = new Properties();
+            FileReader reader = null;
+            try {
+                reader = new FileReader(cfgFile);
+                properties.load(reader);
+            } catch (IOException consumed) {
+                // TODO: Log this error.
+            } finally {
+                if (reader!=null) {
+                    try {
+                        reader.close();
+                    } catch (IOException consumed) {
+                        // TODO: Log this error.
+                    }
+                }
+            }
+            diskStoreDir = properties.getProperty("disk.store.dir", DEFAULT_DISK_STORE);
+        }
+
+        return new File(diskStoreDir);
     }
 
     @Override
