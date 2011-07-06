425daeb889ef3f60ccf46a0f5a610952a839009d
Alexandre Joseph <me@alexandrejoseph.com>
2011-07-06 12:01:00 +0200

    Use lun 1 instead of lun 0 when mounting disk

--- pdisk-host/tar/src/main/scripts/attach-persistent-disk.sh
+++ pdisk-host/tar/src/main/scripts/attach-persistent-disk.sh
@@ -7,7 +7,7 @@ DEVICE_LINK=$2
 DEVICE_MARKER=$DEVICE_LINK.iscsi.uuid
 
 # Hardcoded (for now) portal information. 
-PORTAL_IP=134.158.75.2
+PORTAL_IP=134.158.75.2 # onehost-2.lal.in2p3.fr
 PORTAL_PORT=3260
 PORTAL="$PORTAL_IP:$PORTAL_PORT"
 
@@ -16,7 +16,7 @@ DISK_PREFIX="iqn.2011-01.eu.stratuslab"
 DISK_UUID=`echo $UUID_URL | cut -d : -f 2`
 DISK="$DISK_PREFIX:$DISK_UUID"
 
-DISK_PATH="/dev/disk/by-path/ip-$PORTAL-iscsi-$DISK-lun-0"
+DISK_PATH="/dev/disk/by-path/ip-$PORTAL-iscsi-$DISK-lun-1"
 
 # Must contact the server to discover what disks are available.
 DISCOVER_CMD="$ISCSIADM --mode discovery --type sendtargets --portal $PORTAL"
