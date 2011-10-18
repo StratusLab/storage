3296c07334ce9d7a82b5535e001fc845b9e8707b
Konstantin Skaburskas <konstan@sixsq.com>
2011-10-18 17:47:16 +0200

    Added handy methods to launch a command and get only its return code. Also, collect stdout/stderr output from the process and in case of a falure return it in the exception.

--- pdisk-server/war/src/main/java/eu/stratuslab/storage/disk/utils/ProcessUtils.java
+++ pdisk-server/war/src/main/java/eu/stratuslab/storage/disk/utils/ProcessUtils.java
@@ -1,6 +1,8 @@
 package eu.stratuslab.storage.disk.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 import org.restlet.data.Status;
@@ -17,10 +19,24 @@ public final class ProcessUtils {
     public static void execute(ProcessBuilder pb, String errorMsg) {
         int returnCode = 1;
         Process process;
+        StringBuffer outputBuf = new StringBuffer();
+
+        pb.redirectErrorStream(true);
 
         try {
             process = pb.start();
+
+            BufferedReader stdOutErr = new BufferedReader(new InputStreamReader(
+            		process.getInputStream()));
+    		String line;
+    		while ((line = stdOutErr.readLine()) != null) {
+    			outputBuf.append(line);
+    			outputBuf.append("\n");
+    		}
+
             processWait(process);
+
+            stdOutErr.close();
 
             returnCode = process.exitValue();
         } catch (IOException e) {
@@ -37,14 +53,29 @@ public final class ProcessUtils {
 
         if (returnCode != 0) {
 
+        	process.getErrorStream();
+
             String msg = "An error occurred while executing command: "
-                    + MiscUtils.join(pb.command(), " ") + ".\n" + errorMsg
+                    + MiscUtils.join(pb.command(), " ") + ".\n"
+                    + outputBuf.toString() + "\n"
+                    + errorMsg
                     + ".\nReturn code was: " + String.valueOf(returnCode);
 
             LOGGER.severe(msg);
 
             throw new ResourceException(Status.SERVER_ERROR_INTERNAL, msg);
         }
+    }
+
+
+    public static int executeGetStatus(ProcessBuilder pb) {
+    	Process process;
+    	try {
+	    	process = pb.start();
+	        return processWaitGetStatus(process);
+    	} catch (IOException e) {
+    		return -1;
+    	}
     }
 
     private static void processWait(Process process) {
@@ -60,4 +91,19 @@ public final class ProcessUtils {
 
     }
 
+    private static int processWaitGetStatus(Process process) {
+    	int rc = -1;
+    	boolean blocked = true;
+    	while (blocked) {
+    		try {
+    			rc = process.waitFor();
+    			blocked = false;
+    		} catch (InterruptedException consumed) {
+    			// just continue to wait
+    		}
+    	}
+    	return rc;
+    }
+
 }
+
