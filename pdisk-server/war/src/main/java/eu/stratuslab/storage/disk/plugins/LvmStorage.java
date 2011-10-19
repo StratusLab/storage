bde878979a410617bf5e009a989dca994f45fc36
Marc-Elian Begin <meb@sixsq.com>
2011-10-19 12:51:55 +0200

    Method extracted device path calculation

--- pdisk-server/war/src/main/java/eu/stratuslab/storage/disk/plugins/LvmStorage.java
+++ pdisk-server/war/src/main/java/eu/stratuslab/storage/disk/plugins/LvmStorage.java
@@ -38,10 +38,8 @@ public final class LvmStorage implements DiskStorage {
 
 		String rebasedUuid = DiskUtils.generateUUID();
 
-		String sourcePath = RootApplication.CONFIGURATION.LVM_GROUP_PATH + "/"
-				+ cowUuid;
-		String rebasedPath = RootApplication.CONFIGURATION.LVM_GROUP_PATH + "/"
-				+ rebasedUuid;
+		String sourcePath = DiskUtils.getDevicePath() + cowUuid;
+		String rebasedPath = DiskUtils.getDevicePath() + rebasedUuid;
 		ProcessBuilder pb = new ProcessBuilder("dd", "if=" + sourcePath, "of="
 				+ rebasedPath);
 
@@ -59,8 +57,7 @@ public final class LvmStorage implements DiskStorage {
 		checkDiskExists(baseUuid);
 
 		String lvmSize = size + "G";
-		String sourcePath = RootApplication.CONFIGURATION.LVM_GROUP_PATH + "/"
-				+ baseUuid;
+		String sourcePath = DiskUtils.getDevicePath() + baseUuid;
 		ProcessBuilder pb = new ProcessBuilder(
 				RootApplication.CONFIGURATION.LVCREATE_CMD, "--snapshot", "-p",
 				"rw", "--size", lvmSize, "--name", cowUuid, sourcePath);
@@ -69,8 +66,7 @@ public final class LvmStorage implements DiskStorage {
 	}
 
 	public void delete(String uuid) {
-		String volumePath = RootApplication.CONFIGURATION.LVM_GROUP_PATH + "/"
-				+ uuid;
+		String volumePath = DiskUtils.getDevicePath() + uuid;
 
 		if (false == (new File(volumePath)).exists()) {
 			return;
