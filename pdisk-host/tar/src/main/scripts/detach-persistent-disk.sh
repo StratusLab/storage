546543a75887122f1ac4282549a17e3820a7546c
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2011-11-28 14:50:30 +0100

    Let it be advisory instead of halt-on-error. There is a problem at VM kill/shutdown as detach-persistent-disk.sh is called when VM already stopped. So, when it calls PDISK server to deregister the mount - PDISK server calls detach-persistent-disk.sh again with hot-unplug which fails because VM is not running any more.

--- pdisk-host/tar/src/main/scripts/detach-persistent-disk.sh
+++ pdisk-host/tar/src/main/scripts/detach-persistent-disk.sh
@@ -87,8 +87,10 @@ detach_all_disks() {
 }
 
 detach_hotplug_disk() {
+    set +e 
     sudo /usr/bin/virsh detach-disk one-$VM_ID $TARGET
     detach_${SHARE_TYPE} $UUID_URL
+    set -e
 }
 
 if [ "x$TARGET" = "x" ]
