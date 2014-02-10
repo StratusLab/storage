faff4e4d6f3289de10ed4631defca66e80dbe51f
Konstantin Skaburskas <konstan@sixsq.com>
2014-02-11 00:50:55 +0100

    minor - capitalised constants and extracted args and options parsing.

--- pdisk-server/rpm/src/main/python/persistent-disk-backend.py
+++ pdisk-server/rpm/src/main/python/persistent-disk-backend.py
@@ -33,12 +33,13 @@ from stratuslab.pdiskbackend.ConfigHolder import ConfigHolder
 from stratuslab.pdiskbackend.PdiskBackendProxyFactory import PdiskBackendProxyFactory
 
 # Keys are supported actions, values are the number of arguments required for the each action
-valid_actions = {'check':1, 'create':2, 'delete':1, 'rebase':1, 
+VALID_ACTIONS = {'check':1, 'create':2, 'delete':1, 'rebase':1, 
                  'snapshot':3, 'getturl':1 , 'map':1 , 'unmap':1}
-valid_actions_str = ', '.join(valid_actions.keys())
+VALID_ACTIONS_STR = ', '.join(VALID_ACTIONS.keys())
 
-usage_text = """usage: %prog [options] action_parameters
-
+def parse_args():
+    usage_text = """usage: %prog [options] action_parameters
+    
 Parameters:
     action=check:    LUN_UUID
     action=create:   LUN_UUID LUN_Size
@@ -49,27 +50,28 @@ Parameters:
     action=snapshot: LUN_UUID New_LUN_UUID Snapshot_Size
     action=unmap:    LUN_UUID
 """
-parser = OptionParser(usage=usage_text)
-parser.add_option('--config', dest='config_file', action='store', 
-                  default=defaults.CONFIG_FILE_NAME, 
-                  help='Name of the configuration file to use (D: %s)' %\
-                                                    defaults.CONFIG_FILE_NAME)
-parser.add_option('--action', dest='action', action='store', default='', 
-                  help='Action to execute. Valid actions: %s' % valid_actions_str)
-parser.add_option('-v', '--debug', '--verbose', dest='verbosity',
-                  action='count', default=defaults.VERBOSITY, 
-                  help='Increase verbosity level for debugging (multiple allowed)')
-options, args = parser.parse_args()
+    parser = OptionParser(usage=usage_text)
+    parser.add_option('--config', dest='config_file', action='store', 
+                      default=defaults.CONFIG_FILE_NAME, 
+                      help='Name of the configuration file to use (D: %s)' %\
+                                                        defaults.CONFIG_FILE_NAME)
+    parser.add_option('--action', dest='action', action='store', default='', 
+                      help='Action to execute. Valid actions: %s' % VALID_ACTIONS_STR)
+    parser.add_option('-v', '--debug', '--verbose', dest='verbosity',
+                      action='count', default=defaults.VERBOSITY, 
+                      help='Increase verbosity level for debugging (multiple allowed)')
+    options, args = parser.parse_args()
+    return options, args
 
-
+options, args = parse_args()
 ch = ConfigHolder(config_file_name=options.config_file, 
                   verbosity=options.verbosity)
 logger = Logger(ch)
 
 
-if options.action in valid_actions:
-    if len(args) < valid_actions[options.action]:
-        logger.debug(0, "Insufficient argument provided (%d required)" % valid_actions[options.action])  
+if options.action in VALID_ACTIONS:
+    if len(args) < VALID_ACTIONS[options.action]:
+        logger.debug(0, "Insufficient argument provided (%d required)" % VALID_ACTIONS[options.action])  
         parser.print_help()
         abort("")
 else:
